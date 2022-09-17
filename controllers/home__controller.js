6f8c3b89f84037b8a0ca1226b4d0fd96f20cfcf7
skrajup <skrajprof1947@gmail.com>
2022-09-18 01:26:18 +0530

    added various pages

--- controllers/home__controller.js
+++ controllers/home__controller.js
@@ -2,5 +2,24 @@ const get__index = (req, res)=>{
     res.render("home.ejs");
 }
 
+const get__education = (req, res) => {
+    res.render("education.ejs");
+}
 
-module.exports = { get__index };
+const get__skills = (req, res) => {
+    res.render("skills.ejs");
+}
+
+const get__achievements = (req, res) =>{
+    res.render("achievements.ejs");
+}
+
+const get__interests = (req, res) => {
+    res.render("interests.ejs");
+}
+
+const get__curriculars = (req, res) => {
+    res.render("curriculars.ejs");
+}
+
+module.exports = { get__index, get__education, get__skills, get__achievements, get__interests, get__curriculars };
--- routes/home__routes.js
+++ routes/home__routes.js
@@ -2,5 +2,10 @@ const router = require("express").Router();
 const home__controller = require("../controllers/home__controller");
 
 router.get("/", home__controller.get__index);
+router.get("/education", home__controller.get__education);
+router.get("/skills", home__controller.get__skills);
+router.get("/achievements", home__controller.get__achievements);
+router.get("/interests", home__controller.get__interests);
+router.get("/curriculars", home__controller.get__curriculars);
 
 module.exports =  router;
